975039c5e8a52b49be82c364fad9ac8d9af3ce6a
voka <chdlswhd7@naver.com>
2021-09-28 21:26:18 +0900

    YeAP - nummber Spinner 이식 .

--- bubble-navigation/app/src/main/java/com/gauravk/bubblebarsample/fragment/CalendarFragment.java
+++ bubble-navigation/app/src/main/java/com/gauravk/bubblebarsample/fragment/CalendarFragment.java
@@ -2,6 +2,7 @@ package com.gauravk.bubblebarsample.fragment;
 
 import android.database.Cursor;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,6 +13,7 @@ import android.widget.CalendarView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.gauravk.bubblebarsample.DB.QueryClass;
@@ -25,17 +27,20 @@ import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.CalendarMode;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.shawnlin.numberpicker.NumberPicker;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.Executors;
 
 public class CalendarFragment extends Fragment {
     private QueryClass databaseQueryClass;
     private final OneDayDecorator oneDayDecorator = new OneDayDecorator();
+    private static String TAG = "NumberPicker";
     String time,kcal,menu;
     Cursor cursor;
     SimpleDateFormat sdformat;
@@ -54,6 +59,95 @@ public class CalendarFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
+
+        final NumberPicker numberPicker = getView().findViewById(R.id.number_picker);
+
+        // Set divider color
+        numberPicker.setDividerColor(ContextCompat.getColor(getActivity(), R.color.Spinner));
+        numberPicker.setDividerColorResource(R.color.Spinner);
+
+        // Set formatter
+        numberPicker.setFormatter(getString(R.string.number_picker_formatter));
+        numberPicker.setFormatter(R.string.number_picker_formatter);
+
+        // Set selected text color
+        numberPicker.setSelectedTextColor(ContextCompat.getColor(getActivity(), R.color.Spinner));
+        numberPicker.setSelectedTextColorResource(R.color.Spinner);
+
+        // Set selected text size
+        numberPicker.setSelectedTextSize(getResources().getDimension(R.dimen.selected_text_size));
+        numberPicker.setSelectedTextSize(R.dimen.selected_text_size);
+
+        // Set selected typeface
+        numberPicker.setSelectedTypeface(Typeface.create(getString(R.string.roboto_light), Typeface.NORMAL));
+        numberPicker.setSelectedTypeface(getString(R.string.roboto_light), Typeface.NORMAL);
+        numberPicker.setSelectedTypeface(getString(R.string.roboto_light));
+        numberPicker.setSelectedTypeface(R.string.roboto_light, Typeface.NORMAL);
+        numberPicker.setSelectedTypeface(R.string.roboto_light);
+
+        // Set text color
+        numberPicker.setTextColor(ContextCompat.getColor(getActivity(), R.color.dark_grey));
+        numberPicker.setTextColorResource(R.color.dark_grey);
+
+        // Set text size
+        numberPicker.setTextSize(getResources().getDimension(R.dimen.text_size));
+        numberPicker.setTextSize(R.dimen.text_size);
+
+        // Set typeface
+        numberPicker.setTypeface(Typeface.create(getString(R.string.roboto_light), Typeface.NORMAL));
+        numberPicker.setTypeface(getString(R.string.roboto_light), Typeface.NORMAL);
+        numberPicker.setTypeface(getString(R.string.roboto_light));
+        numberPicker.setTypeface(R.string.roboto_light, Typeface.NORMAL);
+        numberPicker.setTypeface(R.string.roboto_light);
+
+        // Set value
+        numberPicker.setMaxValue(59);
+        numberPicker.setMinValue(0);
+        numberPicker.setValue(3);
+
+        // Set string values
+//        String[] data = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
+//        numberPicker.setMinValue(1);
+//        numberPicker.setMaxValue(data.length);
+//        numberPicker.setDisplayedValues(data);
+
+        // Set fading edge enabled
+        numberPicker.setFadingEdgeEnabled(true);
+
+        // Set scroller enabled
+        numberPicker.setScrollerEnabled(true);
+
+        // Set wrap selector wheel
+        numberPicker.setWrapSelectorWheel(true);
+
+        // Set accessibility description enabled
+        numberPicker.setAccessibilityDescriptionEnabled(true);
+
+        // OnClickListener
+        numberPicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "Click on current value");
+            }
+        });
+
+        // OnValueChangeListener
+        numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                Log.d(TAG, String.format(Locale.US, "oldVal: %d, newVal: %d", oldVal, newVal));
+            }
+        });
+
+        // OnScrollListener
+        numberPicker.setOnScrollListener(new NumberPicker.OnScrollListener() {
+            @Override
+            public void onScrollStateChange(NumberPicker picker, int scrollState) {
+                if (scrollState == SCROLL_STATE_IDLE) {
+                    Log.d(TAG, String.format(Locale.US, "newVal: %d", picker.getValue()));
+                }
+            }
+        });
         Mycalendar = (MaterialCalendarView) getView().findViewById(R.id.mycal);
         Mycalendar.state().edit()
                 .setFirstDayOfWeek(Calendar.SUNDAY)
